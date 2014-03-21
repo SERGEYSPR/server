30fab903ef44ad851371531b49d08bfd3d0c8b56
Сергей Спиридонов <sergey.spr@gmail.com>
2014-03-21 09:27:45 +0400

    server

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.use(app.router);
 app.use(express.bodyParser());
 app.use(express.cookieParser());
 
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 app.get('/', function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
     res.end('Hello!');
