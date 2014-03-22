004d5b1ce0fff70041c5efad1837d83d24ca2ad4
Сергей Спиридонов <sergey.spr@gmail.com>
2014-03-23 00:30:52 +0400

    45

--- server.js
+++ server.js
@@ -75,9 +75,11 @@ app.get('/api/students.get', function (req, res) {
 
         for (var i = 0; i < requiredFields.length; i++)
         {
+            console.log(requiredFields[i]);
             for (var j = 0; j < studentFields.length; j++)
             {
                 if (requiredFields[i] === studentFields[j]) {
+                    console.log('ok');
                     columns += requiredFields[i] + (i === requiredFields.length - 1) ? '' : ',';
                 }
             }
